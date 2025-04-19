5072c0d5ccd35ea75aa3ef360d60c98eae33e0e7
taka727 <taka_10727@icloud.com>
2025-04-20 01:14:46 +0900

    feat : lambda上で動作するように実装

---
+++ sam/slack-gpt-bot-sam/slackFunction/common/gptClient.js
@@ -0,0 +1,18 @@
+const axios = require("axios");
+
+exports.askGPT = async (message) => {
+    const res = await axios.post(
+      "https://api.openai.com/v1/chat/completions",
+      {
+        model: "gpt-4",
+        messages: [{ role: "user", content: message }],
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        },
+      }
+    );
+    return res.data.choices[0].message.content;
+  };
