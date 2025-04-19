bad6470193c7be9147e9281b63943e17df817339
taka727 <taka_10727@icloud.com>
2025-04-20 02:06:08 +0900

    feat : aws-sdk を使って SecureString を取得する

--- sam/slack-gpt-bot-sam/slackFunction/common/gptClient.js
+++ sam/slack-gpt-bot-sam/slackFunction/common/gptClient.js
@@ -1,18 +1,34 @@
 const axios = require("axios");
+const { SSMClient, GetParameterCommand } = require("@aws-sdk/client-ssm");
 
+const ssm = new SSMClient({ region: "ap-northeast-1" });
 exports.askGPT = async (message) => {
-    const res = await axios.post(
-      "https://api.openai.com/v1/chat/completions",
-      {
-        model: "gpt-4",
-        messages: [{ role: "user", content: message }],
+  const openaiApiKey = await getSecretParam("/slack-gpt/openai_api_key");
+
+  const res = await axios.post(
+    "https://api.openai.com/v1/chat/completions",
+    {
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: message }],
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${openaiApiKey}`,
       },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-        },
-      }
-    );
-    return res.data.choices[0].message.content;
-  };
+    }
+  );
+  return res.data.choices[0].message.content;
+};
+
+
+
+async function getSecretParam(name) {
+  const command = new GetParameterCommand({
+    Name: name,
+    WithDecryption: true,
+  });
+
+  const result = await ssm.send(command);
+  return result.Parameter.Value;
+}
