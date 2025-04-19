8d328ada7dace8ed60975104dcf09e384399aa61
taka727 <taka_10727@icloud.com>
2025-04-20 01:14:46 +0900

    feat : lambda上で動作するように実装

---
+++ sam/slack-gpt-bot-sam/slackFunction/app.js
@@ -0,0 +1,67 @@
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ * 
+ */
+
+const axios = require("axios");
+const { askGPT } = require("./common/gptClient");
+
+exports.lambdaHandler = async (event, context) => {
+  try {
+    console.log("Slackイベント：",event);
+
+    const body = typeof event.body === "string" ?  JSON.parse(event.body) : event.body;
+
+    // Slackの初期検証用
+    if (body.type === "url_verification") {
+      return {
+        statusCode: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ challenge: body.challenge }),
+      };
+    }
+
+    if (body.event && body.event.type === "app_mention") {
+      const userMessage = body.event.text.replace(/<@.*?>/g, "").trim();
+      const slackChannel = body.event.channel;
+
+      const gptRes = await askGPT(userMessage);
+
+      // Slack に返信
+      await axios.post(
+        "https://slack.com/api/chat.postMessage",
+        {
+          channel: slackChannel,
+          text: gptRes,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+    return {
+      statusCode: 200,
+      body: "OK",
+    };
+  } catch (error) {
+    console.error("Lambdaエラー:", error.response?.data || error.message);
+    return {
+      statusCode: 500,
+      body: "Internal Server Error",
+    }
+  }
+
+};
